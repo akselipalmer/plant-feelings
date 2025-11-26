943430d0d989c89afbe92ddbe214f5d901ce896b
Akseli Palmer <akseli@toddsden.com>
2025-11-25 22:32:27 -0500

    removed e2e from husky

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Page() {
+  return (
+    <div>
+      <h1 className="text-4xl">About Page</h1>
+      <Link href="/">Home</Link>
+    </div>
+  );
+}
